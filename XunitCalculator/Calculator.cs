84fe06dc238dcfbfa4489c7b862caba53570fc54
Vinicius Oliveira <vinicius.oliveira@crawfordbrasil.com.br>
2022-06-03 09:55:47 -0300

    Adicao Arquivos

---
+++ XunitCalculator/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XunitCalculator
+{
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public int Divide(int number1, int number2)
+        {
+            if (number1 == 0 || number2 == 0)
+            {
+                return 0;
+            }
+            return number1 / number2;
+        }
+
+        public double DivideFloatPoint(double number1, double number2)
+        {
+            return (number1 / number2);
+        }
+
+    }
+}
---
+++ XunitCalculator/CalculatorUnitTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace XunitCalculator
+{
+    public class CalculatorXunitTests
+    {
+        Calculator calculator = new Calculator();
+
+        [Fact]
+        public void Test_is_running_in_linux_container()
+        {
+            Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Linux));
+        }
+
+        [Fact]
+        public void Add_TwoInt_ReturnValue()
+        {
+
+            int num1 = 2;
+            int num2 = 6;
+            int valorEsperado = 8;
+
+            int OperacoaoSoma = calculator.Add(num1, num2);
+
+            Assert.Equal(valorEsperado, OperacoaoSoma);
+
+        }
+
+        [Fact]
+        public void Subtract_TwoInt_ReturnValue()
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int valorEsperado = -5;
+
+            int OperacaoSubtracao = calculator.Subtract(num1, num2);
+            Assert.Equal(valorEsperado, OperacaoSubtracao);
+        }
+
+        [Fact]
+        public void Divide_TwoInt_ReturnValue()
+        {
+            int num1 = 24;
+            int num2 = 12;
+            int valorEsperado = 2;
+
+            int operacaoDivide = calculator.Divide(num1, num2);
+            Assert.Equal(valorEsperado, operacaoDivide);
+        }
+        [Fact]
+        public void Multply_TwoInt_ReturnValue()
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int valorEsperado = 50;
+
+            int operacaoMultiplica = calculator.Multiply(num1, num2);
+            Assert.Equal(valorEsperado, operacaoMultiplica);
+
+        }
+
+        [Fact]
+        public void DivideFloatPoint()
+        {
+            double num1 = 12;
+            double num2 = 5;
+            double valorEsperado = 2.4;
+
+            double Operacoa = calculator.DivideFloatPoint(num1, num2);
+            Assert.Equal(valorEsperado, Operacoa);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 3, 5)]
+        [InlineData(-2, 2, 0)]
+        [InlineData(int.MinValue, -1, int.MaxValue)]
+        public void Add_Theory(int value1, int value2, int expect)
+        {
+            int result = calculator.Add(value1, value2);
+            Assert.Equal(expect, result);
+        }
+    }
+}
+
+
